78ccf19b5e43b6e20199d1cd2f519f394e77ce35
acabutulija <aca.butulija@gmail.com>
2023-11-12 01:30:45 +0100

    [0.1] Napravljen ceo home page, odradjen nav bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonImg,
+  IonMenuButton,
+  IonMenu,
+  IonListHeader, IonNav, IonItem, IonLabel, IonList, IonButton, IonCol, IonRow, IonGrid
+} from '@ionic/angular/standalone';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgOptimizedImage, IonImg, IonMenuButton, IonMenu, IonListHeader, IonNav, IonItem, IonLabel, IonList, IonButton, IonCol, IonRow, IonGrid],
 })
 export class HomePage {
   constructor() {}
