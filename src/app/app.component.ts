81c420f4b0a1334c71d3b8f2df0257483fa26f68
acabutulija <aca.butulija@gmail.com>
2023-11-12 15:01:40 +0100

    [0.2] Izbrisan stari nav-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import {NavBarComponent} from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
