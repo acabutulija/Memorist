9477a9e20c994b4624964f04065dc057f962af26
acabutulija <aca.butulija@gmail.com>
2023-11-13 00:44:32 +0100

    [0.4] Povezana Firebase baza podataka

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiuu1Kxp5U6OKTySziCNuavO2Zq2I2gAg",
+  authDomain: "memoristdb.firebaseapp.com",
+  projectId: "memoristdb",
+  storageBucket: "memoristdb.appspot.com",
+  messagingSenderId: "480924890306",
+  appId: "1:480924890306:web:7f024094d09956b92b375b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
