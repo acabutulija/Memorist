6695de99e3462be9e02925539f18a1d5a635ab44
acabutulija <aca.butulija@gmail.com>
2023-11-12 02:37:42 +0100

    [0.2.1] Dodavanje nav-a kao ionic componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
+import {NavBarComponent} from "../nav-bar/nav-bar.component";
 
 describe('HomePage', () => {
   let component: HomePage;
